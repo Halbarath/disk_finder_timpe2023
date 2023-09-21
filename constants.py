e6effe86e9f695a7d584d2133a02b814e66958d2
Miles Timpe <mtimpe@me.com>
2023-09-21 13:21:08 +0200

    Removed unused constants from constants.py

--- constants.py
+++ constants.py
@@ -5,15 +5,11 @@
 
 # Newton's gravitational constant
 G_SI  = 6.67408e-11  # m3 kg-1 s-2
-#G_CGS = 6.67259e-8   # cm3 g-1 s-2
-#G_EES = 1.536e-6     # RE3 ME-1 s-2
 
 # Mass of Earth
 # https://ssd.jpl.nasa.gov/?planet_phys_par
 MEARTH_KG = 5.97237e24
 MEARTH_G  = 5.97237e27
-
-#M_EARTH2MOON = 0.0123
 
 # Mean radius of the Earth
 # https://ssd.jpl.nasa.gov/?planet_phys_par
@@ -23,34 +19,3 @@ REARTH_CM = 6.3710084e8
 
 # Earth-Moon system values
 J_EM = 3.6e34  # J-s
-
-# Earth-Sun Hill sphere radius
-#R_HILL = 235.2  # Earth radii
-
-# Earth-Moon Roche limit
-#R_ROCHE_RIGID = 1.49  # Earth radii
-#R_ROCHE_FLUID = 2.88  # Earth radii
-
-# Lunar semi-major axis
-#A_MOON = 60.4  # Earth radii
-
-# Conversions
-#GCM_TO_E2E = 0.043444993
-
-#VEL_GAS_TO_KMS = REARTH_KM
-# km/s to Earth radii per second
-#VEL_KMS_TO_GAS = 1.0/REARTH_KM  # RE s-1
-
-
-# Density
-#RHO_GAS2GCM3 = 0.3682
-
-
-# Gasoline internal units
-#GAS_T = 6379.25769387  # seconds
-
-# MEARTH_GAS
-#MEARTH_GAS = 62.366
-
-#dKpcUnit  = 2.06701e-13
-#dMsolUnit = 4.80438e-08
